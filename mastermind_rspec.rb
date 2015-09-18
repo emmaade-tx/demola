49f4935c272c5549e31e2bbbd493e6f78d025c3c
Emmanuel Ade <emmade.tx@gmail.com>
2015-09-18 10:16:39 +0100

    rspec saga

--- mastermind_rspec.rb
+++ mastermind_rspec.rb
@@ -11,11 +11,3 @@ end
       expect(WelcomeMessage.start_message).to eq ()
     end
   end
-  describe "GameEngine" do
-  it "has the same position with the guess code" do
-     GameEngine.play_calc_exact_partial generated_code(["y","r","b","b"])
-     GameEngine.play_calc_exact_partial guess_one(["y","b","b","r"])
-    expect(GameEngine.play_calc_exact_partial partial_match).to eq [3]
-    expect(GameEngine.play_calc_exact_partial exact_match).to eq [1]
-  end
-end
